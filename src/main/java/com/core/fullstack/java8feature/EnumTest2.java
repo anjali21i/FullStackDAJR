b86d6f549ca7d80d296933f64a9a91d7515037c0
Anjali <31880656+anjali21i@users.noreply.github.com>
2023-08-14 16:58:14 +0530

    Create EnumTest2.java

---
+++ src/main/java/com/core/fullstack/java8feature/EnumTest2.java
@@ -0,0 +1,21 @@
+enum EnumTest2 {
+	SMALL("The size is small."),
+	MEDIUM("The size is medium."),
+	LARGE("The size is large."),
+	EXTRALARGE("The size is extra large.");
+
+	private final String pizzaSize;
+
+	private EnumTest2(String pizzaSize) {
+		this.pizzaSize = pizzaSize;
+	}
+
+	public String getSize() {
+		return this.pizzaSize;
+	}
+	
+	public static void main(String[] args) {
+		EnumTest2 size = EnumTest2.SMALL;
+		System.out.println(size.getSize());
+	}
+}
