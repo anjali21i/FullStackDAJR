2f5015690707639c6bb3994c281a25e32f9156ea
Anjali <anjaliguptajnp21@gmail.com>
2023-08-23 02:45:48 +0530

    init

---
+++ src/main/java/com/core/fullstack/J2SE/ExceptionsPractice.java
@@ -0,0 +1,13 @@
+package com.core.fullstack.J2SE;
+
+public class ExceptionsPractice {
+	
+	//runtime
+	//ioException / compile time
+	
+	//arrayoutofboundException
+	//nullpointer
+	//ArithmeticException
+	//IllegalArgumentException
+
+}
