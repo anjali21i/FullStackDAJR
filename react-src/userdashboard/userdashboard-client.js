b6a814e4d76da67f25a9889ea774e413dd6b3b9d
Anjali <anjaliguptajnp21@gmail.com>
2023-12-11 10:48:00 +0530

    Update userdashboard-client.js

--- react-src/userdashboard/userdashboard-client.js
+++ react-src/userdashboard/userdashboard-client.js
@@ -3,9 +3,11 @@ const { PersistGate } = require("redux-persist/integration/react");
 const { PROJECT_ID_MAP } = require("./utils");
 const { configureStore } = require("@reduxjs/toolkit");
 const { RouterProvider } = require("react-router-dom");
+const preloadedState = window.__PRELOADED_STATE__;
+
 
 ReactDOM.hydrate((
-    <RouterProvider store={configureStore()}>
+    <RouterProvider store={configureStore(), }>
         <PersistGate>
 
         </PersistGate>
