56a6c0bda301b471006d2894c798f141e172645a
Anjali Gupta <anjaliguptajnp21@gmail.com>
2023-12-14 01:39:06 +0530

    redux setup
    Signed-off-by: Anjali Gupta <anjaliguptajnp21@gmail.com>

--- react-src/userdashboard/userdashboard-client.js
+++ react-src/userdashboard/userdashboard-client.js
@@ -1,26 +1,14 @@
-
-import createStore from './createReduxStore'
 import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router'
-import thunk from 'redux-thunk';
-import { applyMiddleware } from 'redux';
 import RootComponent from './RootComponent';
+import { BrowserRouter } from 'react-router-dom';
+import { clientStore, persistor } from './Redux/store/storeConfig';
 const { PersistGate } = require("redux-persist/integration/react");
 const { PROJECT_ID_MAP } = require("./utils");
-const { configureStore } = require("@reduxjs/toolkit");
 const preloadedState = window.__PRELOADED_STATE__;
 
-const store1 = createStore()
-const { store2, persistor } = createStore(initialState, browserHistory);
-const store3 = createStore(rootReducer, applyMiddleware(thunk));
+const initialState = {
 
-
-const clientStore = configureStore({
-    reducer: rootReducer,
-    preloadedState,
-})
-
-const initialState = {}
+}
 
 ReactDOM.hydrate(
     (csrReactComponent), document.getElementById(PROJECT_ID_MAP.USER_DASHBOARD)
@@ -29,7 +17,9 @@ ReactDOM.hydrate(
 const csrReactComponent = () => (
     <Provider clientStore={clientStore} serverStore={preloadedState}>
         <PersistGate loading={null} persistor={persistor}>
-            <RootComponent />
+            <BrowserRouter>
+                <RootComponent />
+            </BrowserRouter>
         </PersistGate>
     </Provider>
 );
