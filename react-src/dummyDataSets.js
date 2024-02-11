046420028c7ab121912b7109085f688b509dac84
Anjali Gupta <anjaliguptajnp21@gmail.com>
2024-02-11 15:14:24 +0530

    data set

---
+++ react-src/dummyDataSets.js
@@ -0,0 +1,92 @@
+const numbArray = [2, 5, 6, 2, 4];
+const studentArray = ["ritu", "geeta", "priya", "vishu"];
+
+const userRecArray2 = [
+    ["Sandra Schaefer", "Bethany12@yahoo.com", "dipti"],
+    ["Mark Veum", "Jedediah.Beer26@yahoo.com"]
+]
+
+const userRecordArray = [
+    {
+        "name": "Sandra Schaefer",
+        "email": "Bethany12@yahoo.com",
+        "postCode": "65665",
+        "city": "Palm Harbor",
+        "country": "Slovakia (Slovak Republic)",
+        "phoneNumber": "379-607-0449",
+        "favouriteQuote": "Qui consectetur repellendus commodi."
+    },
+    {
+        "name": "Mark Veum",
+        "email": "Jedediah.Beer26@yahoo.com",
+        "postCode": "89859",
+        "city": "Montebello",
+        "country": "Myanmar",
+        "phoneNumber": "779.494.2905 x0531",
+        "favouriteQuote": "Corrupti assumenda enim alias suscipit maxime molestiae quis laboriosam."
+    },
+    {
+        "name": "Ritu Schaefer",
+        "email": "Ritu56@yahoo.com",
+        "postCode": "64665",
+        "city": "Palm Harbor",
+        "country": "Slovakia (Slovak Republic)",
+        "phoneNumber": "979-607-0449",
+        "favouriteQuote": "Qui consectetur repellendus commodi."
+    }
+]
+
+
+const userRecArray = [
+    {
+        id:1,
+        name: "Sandra Schaefer",
+        email: "Bethany12@yahoo.com",
+        city: "Palm munich",
+        phone: "379-607-0449"
+    },
+    {
+        id:2,
+        name: "Urr Veum",
+        email: "Jedediah.Beer26@yahoo.com",
+        city: "Montebello",
+        phone: "899-607-0449",
+    },{
+        id:3,
+        name: "Sandra Schaefer",
+        email: "War@yahoo.com",
+        city: "Palm Harbor",
+        phone: "909-607-0449"
+    },
+    {
+        id:4,
+        name: "Mark Veum",
+        email: "iuui.Beer26@yahoo.com",
+        city: "Montebello",
+        phone: "459-607-0449",
+    }
+]
+
+console.log(userRecArray);
+
+
+let users = {
+    1: {
+      id: '1',
+      firstName: 'Robin',
+      lastName: 'Wieruch',
+      isDeveloper: true,
+    },
+    2: {
+      id: '2',
+      firstName: 'Dave',
+      lastName: 'Davddis',
+      isDeveloper: false,
+    },
+};
+
+let usersArr = [users];
+
+
+//operation over array
+
