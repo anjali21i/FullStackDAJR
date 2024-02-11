0ed4b2b3f6d84db664bb5b4d81fa1d86cc7923c4
Anjali Gupta <anjaliguptajnp21@gmail.com>
2024-02-11 15:30:28 +0530

    update

--- react-src/practiceJS.js/shadow.js
+++ react-src/practiceJS.js/shadow.js
@@ -1,8 +1,10 @@
 let a = 8;
 // let a = 87;   //gives err block is same
+// var a = 87;   //gives err as shadowd as block
 
 var b = 4;
 let b = 4;
+let b = 7;  //not give error
 
 let c = 89;
 // var c = 8;     //error at c as => shadowing done as its blocked scoped in first declaration 
