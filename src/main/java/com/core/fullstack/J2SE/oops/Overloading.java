bf14fc64ea790ab1a8d611d174f6fa5ab1c3ceae
Anjali <anjaliguptajnp21@gmail.com>
2024-04-21 17:55:19 +0530

    Update Overloading.java

--- src/main/java/com/core/fullstack/J2SE/oops/Overloading.java
+++ src/main/java/com/core/fullstack/J2SE/oops/Overloading.java
@@ -1,25 +1,84 @@
 package com.core.fullstack.J2SE.oops;
 
+class Overload {
+	/////////////////////////////////////
+	
+	public static void getDetail(int val) {
+		System.out.println("int obj: "+ val);
+	}
+	
+	public static void getDetail(float val) {
+		System.out.println("float obj: "+ val);
+	}
+
+	//////////////////////////////////////
+	
+	public static void getDetail(Long val) {             //1
+		System.out.println("Long obj: "+ val);
+	}
+	
+	public static void getDetail(String val) {
+		System.out.println("String obj: "+ val);
+	}
+	
+	public static void getDetail(Object val) {   //not give error
+		System.out.println("Object Obj: "+ val);
+	}
+	
+	public static void getDetail(StringBuffer val) {   //not give error
+		System.out.println("StringBuffer Obj: "+ val);
+	}
+	
+	///////////////////////////////////////////
+	public static void getDetail(int val, float f) {   //not give error
+		System.out.println("int float ver");
+	}
+	
+	public static void getDetail(float f, int val) {   //not give error
+		System.out.println("float int ver");
+	}
+	
+	///////////////////////////////////////////////////
+	
+	public static void getDetailVal(int val) {   //
+		System.out.println("int ver");
+	}
+	
+	public static void getDetailVal(int... val) {   //var arg   least priority  act as default case
+		System.out.println("var arg ver");
+	}
+	
+	
+	
+	
+}
 public class Overloading {
 	
 	public static void main(String arg[]) {
-//		getDetail(9L);
+		Overload ov = new Overload();
+		ov.getDetail(9L);
+		ov.getDetail(9);
+//		ov.getDetail(null); //give CTE
 		
-//		getDetail(null); give CTE
+		
+//		ov.getDetail(2,2.2);	//cannt find for double
+//		ov.getDetail(2.3,2);	//cannt find for double
+		
+		ov.getDetail(2,2.2f);
+		ov.getDetail(2.3f,2);
+//		ov.getDetail(2,2);   // ce ambiguous
+		
+		
+		///////////////////////////////
+		 
+		ov.getDetailVal(9);  //
+		ov.getDetailVal();  //var arg
+		ov.getDetailVal(9, 0, 7);  //var arg 
 		
 	}
 	
-	private static void getDetail(Long val) {
-		System.out.println("Long: "+ val);
-	}
 	
-	private static void getDetail(String val) {
-		System.out.println("String: "+ val);
-	}
 	
-	private static void getDetail(Object val) {   //not give error
-		System.out.println("Obj: "+ val);
-	}
 	
 
 }
