18e26766bea1f4bf0106e903c7c1bdd878b11fd0
Anjali <anjaliguptajnp21@gmail.com>
2024-04-23 23:53:17 +0530

    Create CustomException.java

---
+++ src/main/java/com/core/fullstack/J2SE/exceptionhandling/CustomException.java
@@ -0,0 +1,25 @@
+package com.core.fullstack.J2SE.exceptionhandling;
+
+class MyException extends RuntimeException {
+    MyException(String s) {
+        super(s);
+    }
+}
+
+class NotFoundException extends RuntimeException {
+    NotFoundException(String s) {
+        super(s);
+    }
+}
+
+public class CustomException {
+
+    public static void main(String[] args) {
+        String notFound = "404";
+        if(notFound.equals("404")) {
+            throw new NotFoundException("Page not found!");
+        } else {
+            System.out.println("FOUND");
+        }
+    }
+}
