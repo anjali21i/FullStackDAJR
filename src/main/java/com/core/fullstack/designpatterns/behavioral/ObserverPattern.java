4be2276594dd21ad0535ab2e5a0acda6629940b1
Anjali <anjaliguptajnp21@gmail.com>
2024-04-27 14:04:10 +0530

    Update ObserverPattern.java

--- src/main/java/com/core/fullstack/designpatterns/behavioral/ObserverPattern.java
+++ src/main/java/com/core/fullstack/designpatterns/behavioral/ObserverPattern.java
@@ -1,7 +1,26 @@
-import com.core.fullstack.designpatterns.behavioral;
+package com.core.fullstack.designpatterns.behavioral;
 
 
+interface Observer {
+
+    
+}
+
+class ObserverImpl implements Observer {
+
+}
+
+
+interface Observable {
+
+    
+}
+
+class ObservableImpl {
+
+}
 public class ObserverPattern {
+    //one to many relationship
 
   
 }
