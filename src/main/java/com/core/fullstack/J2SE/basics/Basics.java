ce24390e8e0e43395b691821a10a1f53d6994e4a
anjali21i <anjaliguptajnp21@gmail.com>
2025-06-05 01:21:15 +0530

    final keyword uses

--- src/main/java/com/core/fullstack/J2SE/basics/Basics.java
+++ src/main/java/com/core/fullstack/J2SE/basics/Basics.java
@@ -1,4 +1,31 @@
 package com.core.fullstack.J2SE.basics;
-
+/*
+* final keyword learning
+*/
 public class Basics {
+    private final int test;
+    private static final int test2;
+    {
+        test = 10;
+    }
+
+    static {
+        test2 = 10;
+    }
+
+    public int getTestVal() {
+        return test;
+    }
+    public static void main(String[] args) {
+        System.out.println("test2:"+ test2);
+        Basics basics = new Basics();
+        System.out.println("Number is greater than 5"+ basics.test);
+        System.out.println("Number is greater than 5"+ basics.getTestVal());
+    }
+}
+
+final class NonOverrideNonExtendable {
+    public void test() {
+        System.out.println("This is a final method");
+    }
 }
